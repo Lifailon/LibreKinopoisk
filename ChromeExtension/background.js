7cbd347ea2e2013aed8c16a3c16deb26c3b64077
Lifailon <deadepicboss@gmail.com>
2024-08-20 17:55:38 +0300

    added script for initializing default values

---
+++ ChromeExtension/background.js
@@ -0,0 +1,25 @@
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.local.get(['torSrv', 'KinoboxCheckBox', 'YouTubeCheckBox', 'WikiCheckBox', 'DbEnCheckBox', 'DbRuCheckBox', 'TorrentCheckBox'], function(data) {
+        if (typeof data.torSrv === 'undefined') {
+            chrome.storage.local.set({ 'torSrv': 'https://toruapi.vercel.app' });
+        }
+        if (typeof data.KinoboxCheckBox === 'undefined') {
+            chrome.storage.local.set({ 'KinoboxCheckBox': true });
+        }
+        if (typeof data.YouTubeCheckBox === 'undefined') {
+            chrome.storage.local.set({ 'YouTubeCheckBox': true });
+        }
+        if (typeof data.WikiCheckBox === 'undefined') {
+            chrome.storage.local.set({ 'WikiCheckBox': true });
+        }
+        if (typeof data.DbEnCheckBox === 'undefined') {
+            chrome.storage.local.set({ 'DbEnCheckBox': true });
+        }
+        if (typeof data.DbRuCheckBox === 'undefined') {
+            chrome.storage.local.set({ 'DbRuCheckBox': true });
+        }
+        if (typeof data.TorrentCheckBox === 'undefined') {
+            chrome.storage.local.set({ 'TorrentCheckBox': true });
+        }
+    });
+});
