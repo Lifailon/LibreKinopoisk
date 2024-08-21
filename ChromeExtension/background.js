598e5a8d92bd0d4e94ad70b2e6e90233de23adc9
Lifailon <deadepicboss@gmail.com>
2024-08-21 14:44:12 +0300

    disabling the extension interface on system tabs

--- ChromeExtension/background.js
+++ ChromeExtension/background.js
@@ -1,3 +1,4 @@
+// Значения по умолчанию при установке расширения
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.local.get([
         'torSrv',
@@ -29,6 +30,34 @@ chrome.runtime.onInstalled.addListener(function() {
         }
         if (typeof data.TorrentCheckBox === 'undefined') {
             chrome.storage.local.set({ 'TorrentCheckBox': true })
+        }
+    })
+})
+
+// Отключение интерфейса расширения на системных вкладках
+
+function isSystemPage(url) {
+    return url.startsWith("chrome://") || url.startsWith("chrome-extension://") || url.includes("kinopoisk") 
+}
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete' && tab.url) {
+        if (isSystemPage(tab.url)) {
+            chrome.action.disable(tabId)
+        } else {
+            chrome.action.enable(tabId)
+        }
+    }
+})
+
+chrome.tabs.query({}, (tabs) => {
+    tabs.forEach(tab => {
+        if (tab.url) {
+            if (isSystemPage(tab.url)) {
+                chrome.action.disable(tab.id)
+            } else {
+                chrome.action.enable(tab.id)
+            }
         }
     })
 })
